0cccc6b354baa6ef55a5ce7fddeaf8fda8dc82de
unknown <arifur123@gmail.com>
2019-09-09 20:48:47 +0600

    empty

--- application/views/member/common/inner_footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="gx-footer">
-                <div class="d-flex flex-row justify-content-between">
-                    <p> Copyright seagull publications © <?= date('Y') ?></p>
-                
-                </div>
-            </footer>
--- assets/dashboard/js/custom/mail.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    
-    $('.module-list-info').on('click', function () {
-        window.location = "mail-inner.html";
-    });
-
-})(jQuery);
